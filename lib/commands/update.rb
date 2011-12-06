a0790fa49d1469f138566df6444606f58b46a21b
Sean Grove <s@bushi.do>
2011-12-06 00:17:45 -0800

    Adds the update command, which just updates the local .bushido/tane.yml file

---
+++ lib/commands/update.rb
@@ -0,0 +1,11 @@
+class Tane::Commands::Update < Tane::Commands::Base
+  def self.process(args)
+    if in_rails_dir?
+      Tane::Helpers::Init.update_app
+    else
+      term.say("You have to be in the local rails app's root directory when running `tane update`")
+
+      exit 1
+    end
+  end
+end
