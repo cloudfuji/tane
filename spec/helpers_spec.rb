b8de0416c45452809c9b27a679d4bd96c6d93040
Akash Manohar J <akash@akash.im>
2011-12-07 00:59:03 +0530

    All specs passing for lib/tane/helpers.rb
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -7,7 +7,7 @@ describe "Tane::Helpers" do
   end
   
   it "included should extend ClassMethods" do
-    Tane::Helpers::Example.respond_to?(:term).should be_true
+    Tane::Helpers::Example.include?(Tane::Helpers).should be_true
   end
   
   describe "ClassMethods" do
@@ -20,145 +20,193 @@ describe "Tane::Helpers" do
 
     describe "verbose_say" do
       it "should display the message if the verbose option is true" do
-        pending
+        Tane::Helpers::Example.opts.verbose = true
+        Tane::Helpers::Example.term.should_receive(:say)
+        Tane::Helpers::Example.verbose_say("detailed message")
       end
     end
 
     describe "bushido_dir" do
       it "should return the path of the user's .bushido directory" do
-        pending
+        Tane::Helpers::Example.bushido_dir.should == "#{ENV['HOME']}/.bushido"
       end
     end
 
     describe "email_template_file_path" do
       it "should return path to current project's .bushido/emails.yml file" do
-        pending
+        Tane::Helpers::Example.email_template_file_path.should == ".bushido/emails.yml"
       end
     end
 
     describe "tane_file_path" do
       it "should return path to xsycurrent project's .bushido/tane.yml" do
-        pending
+        Tane::Helpers::Example.tane_file_path == ".bushido/tane.yml"
       end
     end
 
     describe "credentials_file_path" do
       it "should return path to user's Bushido credentials file" do
-        pending
+        Tane::Helpers::Example.credentials_file_path == "#{ENV['HOME']}/.bushido/credentials.yml"
       end
     end
 
     describe "logged_in?" do
       it "should return true if the credentials file exists" do
-        pending
+        File.should_receive(:exists?).and_return(true)
+        Tane::Helpers::Example.logged_in?.should be_true
       end
 
       it "should return false if the credentials file does not exist" do
-        pending
+        File.should_receive(:exists?).and_return(false)
+        Tane::Helpers::Example.logged_in?.should be_false
       end
     end
 
     describe "in_rails_dir?" do
-      it "it should return true if the current dir has a rails or script directory" do
-        pending
+      it "it should return true if the current dir has a rails dir" do
+        Dir.should_receive(:exists?).at_least(1).and_return(true)
+        Tane::Helpers::Example.in_rails_dir?.should be_true
+      end
+
+      it "should return false if the current dir does not have both script and rails dir" do
+        Dir.should_receive(:exists?).at_least(1).and_return(false)
+        Tane::Helpers::Example.in_rails_dir?.should be_false
       end
     end
 
     describe "bushido_app_exists?" do
       it "it should return true if the .bushido dir exists in the current directory" do
-        pending
+        Dir.should_receive(:exists?).and_return(true)
+        Tane::Helpers::Example.bushido_app_exists?.should be_true
+      end
+
+      it "it should return false if the .bushido dir exists in the current directory" do
+        Dir.should_receive(:exists?).and_return(false)
+        Tane::Helpers::Example.bushido_app_exists?.should be_false
       end
     end
 
     describe "username" do
       it "should return the username from the credentials" do
-        pending
+        Tane::Helpers::Example.should_receive(:credentials).and_return({:username=>"username"})
+        Tane::Helpers::Example.username.should == "username"
       end
     end
 
     describe "password" do
       it "should return the password from the credentials" do
-        pending
+        Tane::Helpers::Example.should_receive(:credentials).and_return({:password=>"password"})
+        Tane::Helpers::Example.password.should == "password"
       end
     end
 
     describe "make_global_bushido_dir" do
       it "should create a .bushido dir in the user's $HOME directory" do
-        pending
+        FileUtils.should_receive(:mkdir_p).with("#{ENV['HOME']}/.bushido")
+        Tane::Helpers::Example.make_global_bushido_dir
       end
     end
 
     describe "make_app_bushido_dir" do
       it "should create a .bushido dir in the current dir if it does not have one" do
-        pending
+        Tane::Helpers::Example.should_receive(:bushido_app_exists?).and_return(false)
+        FileUtils.should_receive(:mkdir_p).with(".bushido")
+        Tane::Helpers::Example.make_app_bushido_dir
       end
 
       it "should *not* create a .bushido in the current dir if it already has not" do
-        pending
+        Tane::Helpers::Example.should_receive(:bushido_app_exists?).and_return(true)
+        expect {
+          Tane::Helpers::Example.make_app_bushido_dir
+        }.to raise_error(SystemExit)
       end
     end
 
     describe "save_credentials" do
       it "should save the username and the password in the user's bushido_dir" do
+        File.should_receive(:open)
+        Tane::Helpers::Example.save_credentials("email", "auth_token")
       end
     end
 
     describe "prompt_for_credentials" do
       it "should promt the user for credentials using HighLine" do
-        pending
+        Tane::Helpers::Example.term.should_receive(:ask).twice.and_return("test")
+        Tane::Helpers::Example.prompt_for_credentials
       end
 
       it "should return an array with email and password" do
-        pending
+        Tane::Helpers::Example.term.should_receive(:ask).twice.and_return("test")
+        Tane::Helpers::Example.prompt_for_credentials.should be_kind_of(Array)
       end
     end
 
     describe "warn_if_credentials" do
-      it "should warn that the user is already logged in" do
-        pending
+
+      before :each do
+        Tane::Helpers::Example.should_receive(:logged_in?).and_return(true)
+        Tane::Helpers::Example.should_receive(:username).and_return("test_user")
+      end
+      
+      it "should warn that the user is already logged in if the credentials file exists" do
+
+        Tane::Helpers::Example.term.should_receive(:agree).and_return(true)
+        Tane::Helpers::Example.warn_if_credentials
       end
       
       it "should skip if the user says no to reset credentials" do
-        pending
+        Tane::Helpers::Example.term.should_receive(:agree).and_return(false)
+        expect {
+          Tane::Helpers::Example.warn_if_credentials
+        }.to raise_error(SystemExit)
       end
 
-      it "should return true if the user says yes to reset credentials" do
-        pending
+      it "should return if the user says yes to reset credentials" do
+        Tane::Helpers::Example.term.should_receive(:agree).and_return(true)
+        Tane::Helpers::Example.warn_if_credentials
       end
     end
 
     describe "bushido_envs" do
       it "should return the read the data in .bushido/tane.yml and return a hash" do
-        pending
+        File.should_receive(:read).and_return('HOSTING_PLATFORM: developer')
+        Tane::Helpers::Example.bushido_envs.should == {"HOSTING_PLATFORM"=> "developer"}
       end
     end
 
     describe "base_url" do
       it "should return http://localhost:3000 by default if the --host, --scheme and --port options are not set on the command line" do
-        pending
+        Tane::Helpers::Example.base_url == "http://localhost:3000"
       end
     end
 
     describe "mail_url" do
       it "should return url to local app's Bushido::Mail#index action at/bushido/mail" do
-        pending
+        mail_url = "#{::Tane::Helpers::Example.base_url}/bushido/mail"
+        Tane::Helpers::Example.mail_url.should == mail_url
       end
     end
 
     describe "post" do
+      before :each do
+        Tane::Helpers::Example.should_receive(:bushido_envs).and_return({"BUSHIDO_APP_KEY"=>"abc123"})
+        @data = {}
+        @url = "http://example.com"
+        RestClient.should_receive(:put).with(@url, @data, :content_type => :json, :accept => :json).and_return(@data.to_json)
+      end
+
       it "should post to the given url with the given data" do
-        # if RestClient is stubbed with should_receive(:post)
-        # and added to a before(:each) block then this spec
-        # wont be necessary
-        pending
+        Tane::Helpers::Example.post(@url, @data)
       end
 
       it "should display the result on the terminal if the verbose option is set" do
-        pending
+        Tane::Helpers::Example.opts.verbose=true
+        Tane::Helpers::Example.term.should_receive(:say).at_least(1)
+        Tane::Helpers::Example.post(@url, @data)
       end
       
-      it "should return json" do
-        pending
+      it "should return a hash" do
+        Tane::Helpers::Example.post(@url, @data).should be_kind_of(Hash)
       end
     end
   end
