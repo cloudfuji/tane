b13a4c3fe872b8ff8a2d06e4daee2d6626c593b4
Akash Manohar J <akash@akash.im>
2011-12-06 21:11:22 +0530

    deletes the test_apps dir
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- test_apps/dummy31/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy31::Application.config.secret_token = '0cab7c423169d22c58c01822b167b6d032c27fa55211303339367826fc355d77887dfd24f7e2447824c4c6cae09d807ee1747d27d1abc9adffbbc6d690d570f0'
