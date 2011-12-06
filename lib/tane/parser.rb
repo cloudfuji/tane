100bb41d7b6dad78376a85e256db5608aaf009d5
Akash Manohar J <akash@akash.im>
2011-12-06 20:00:11 +0530

    Moves Tane::Parser module from lib/tane.rb to lib/tane/parser.rb
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ lib/tane/parser.rb
@@ -0,0 +1,56 @@
+module Tane
+  class Parser
+    class << self
+
+      def global_options
+        @global_options ||= []
+      end
+
+      def global_option(name, *args)
+        self.global_options << {:name => name, :args => args}
+      end
+
+      def parse(args)
+        # The options specified on the command line will be collected in *options*.
+        # We set default values here.
+        options = OpenStruct.new
+        options.scheme = "http"
+        options.host = "localhost"
+        options.port = 3000
+        options.inplace = false
+        options.encoding = "utf8"
+        options.transfer_type = :auto
+        options.verbose = false
+
+        global_option :port,    '-p', '--port PORT',     Integer, "The port your local Bushido app is running on"
+        global_option :host,    '-n', '--host HOST',     String,  "The hostname where your local Bushido app is running"
+        global_option :scheme,  '-s', '--scheme SCHEME', String,  "Either http or https, whichever protocol your local Bushido app is using"
+        global_option :verbose, '-V', '--verbose',                "Output a lot of noise"
+
+        opts = OptionParser.new do |opts|
+          banner  = "Usage: tane command [options]\n"
+          banner += "\n"
+          banner += "The commands I know are:\n"
+          ($commands - ["base", "app"]).each do |command|
+            banner += "  #{command}\n"
+          end
+
+          opts.banner = banner
+
+          opts.separator ""
+          opts.separator "Specific options:"
+
+          # Mandatory argument.
+          global_options.each do |option|
+            opts.on(option[:name].to_s, *option[:args]) do |value|
+              options.send("#{option[:name]}=", value)
+            end
+          end
+
+          opts.parse!(args)
+          return options
+        end
+      end
+    end
+  end
+end
