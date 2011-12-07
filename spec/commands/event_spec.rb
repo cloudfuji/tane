5cb872fea290d0f4b3587db2e755fc3b94ce9de2
Akash Manohar J <akash@akash.im>
2011-12-07 15:11:03 +0530

    Spec for Tane::Command::Event
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/commands/event_spec.rb
+++ spec/commands/event_spec.rb
@@ -4,7 +4,17 @@ describe Tane::Commands::Event do
 
   describe ".process" do
     it "should post data to the local apps /bushido/data with the event" do
-      pending
+      args = ["Test", "received", "{'foo' => 'bar'}"]
+      event = {
+        'category' => args.first,
+        'event'    => args[1],
+        'data'     => eval(args[2])
+      }
+
+      Tane::Commands::Event.should_receive(:post).
+        with(Tane::Commands::Event.data_url, event)
+
+      Tane::Commands::Event.process(args)
     end
   end
 end
