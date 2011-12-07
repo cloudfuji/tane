28074ac8ad5e17f6d335f740440f9f4a75b8d556
Akash Manohar J <akash@akash.im>
2011-12-07 14:43:09 +0530

    Specs for Tane::Commands::Claim
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/commands/claim_spec.rb
+++ spec/commands/claim_spec.rb
@@ -3,15 +3,31 @@ require "spec_helper"
 describe Tane::Commands::Claim do
 
   describe ".process" do
-    it "should notify the local app of an App.claimed event" do
-      pending
+
+    before :each do
+      @args = ["valid_email", "valid_ido_id"]
+    end
+    
+    it "should notify the local app about an App.claimed event" do
+      Time.should_receive(:now).at_least(1).and_return("valid_time")
+
+      event = {
+        'category' => 'app',
+        'event'    => 'claimed',
+        'data'     => {
+          'time'   => Time.now,
+          'ido_id' => @args.last,
+          'email' => @args.first
+        }
+      }
+
+      Tane::Commands::Claim.should_receive(:post).
+        with(Tane::Commands::Claim.data_url, event)
+
+      Tane::Commands::Claim.process(@args)
     end
 
-    it "event data should contain time, ido_id and email" do
-      pending
-    end
   end
-
 end
 
   
