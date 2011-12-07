03ca390231b6ffba6a0787dde63edb3a3494b98d
Akash Manohar J <akash@akash.im>
2011-12-07 17:30:56 +0530

    Specs for Tane::Commands::Support
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/commands/support_spec.rb
+++ spec/commands/support_spec.rb
@@ -2,24 +2,31 @@ require "spec_helper"
 
 describe Tane::Commands::Support do
 
+  before :each do
+    @message = "user_entered_message"
+  end
+  
   describe ".process" do
-    it "should prompt the user for the message" do
-      pending
-    end
+    it "should prompt the user for the message and send it to Bushido" do
+      Tane::Commands::Support.term.should_receive(:ask).and_return(@message)
+      Tane::Commands::Support.should_receive(:send_message_to_bushido).with(@message)
 
-    it "should send the specified message to Bushido" do
-      pending
+      Tane::Commands::Support.process([])
     end
   end
 
 
   describe ".send_message_to_bushido" do
     it "should display the message sent" do
-      pending
+      Tane::Commands::Support.should_receive(:username).
+        and_return("valid_username")
+      Tane::Commands::Support.term.should_receive(:say).at_least(1)
+
+      Tane::Commands::Support.send_message_to_bushido(@message)
     end
 
     it "should send a message to Bushido team" do
-      pending
+      pending "send_message_to_bushido doesn't send emails right now"
     end
   end
 end
