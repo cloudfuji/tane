f3b9476ab41bb76077c24c34b4e91da8adc474ef
Akash Manohar J <akash@akash.im>
2011-12-07 17:13:48 +0530

    Specs for Test::Command::Init; Test::Command::Help and Test::Command::Login; Refactors Tane::Command::Login
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- spec/commands/help_spec.rb
+++ spec/commands/help_spec.rb
@@ -4,7 +4,8 @@ describe Tane::Commands::Help do
 
   describe ".process" do
     it "should display the OptParse's help section's banner" do
-      pending
+      Tane::Commands::Help.opts.should_receive(:banner)
+      Tane::Commands::Help.process([])
     end
   end
 
--- spec/commands/init_spec.rb
+++ spec/commands/init_spec.rb
@@ -4,11 +4,21 @@ describe Tane::Commands::Init do
 
   describe ".process" do
     it "should initialize an app if it's a rails project" do
-      pending
+      Tane::Commands::Init.should_receive(:in_rails_dir?).and_return(true)
+      Tane::Helpers::Init.should_receive(:initialize_app)
+
+      Tane::Commands::Init.process([])
     end
 
     it "should display a message and exit if the current dir isn't a rails project" do
-      pending
+      Tane::Commands::Init.should_receive(:in_rails_dir?).
+        and_return(false)
+
+      Tane::Commands::Init.term.should_receive(:say)
+
+      expect {
+        Tane::Commands::Init.process([])
+      }.to raise_error(SystemExit)
     end
   end
 
