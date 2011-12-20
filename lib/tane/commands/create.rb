27ac985cb3a009634c529e7fc30d6b263b485985
Kev Zettler <kevzettler@gmail.com>
2011-12-19 16:25:05 -0800

    updated creation command to be rails 3 syntax

--- lib/tane/commands/create.rb
+++ lib/tane/commands/create.rb
@@ -3,6 +3,6 @@ class Tane::Commands::Create < Tane::Commands::Base
     authenticate_user
     app_name = args[0] ||= term.ask("Please enter a name for your new app:     ") { |app_name| app_name }
 
-    system "bundle exec rails #{app_name} -m https://raw.github.com/Bushido/kimono/master/kimono.rb"
+    system "bundle exec rails new #{app_name} -m https://raw.github.com/Bushido/kimono/master/kimono.rb"
   end
 end
